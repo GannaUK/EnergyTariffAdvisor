081b8a7fae7141a7e53121461c447fc4cb7c159f
GannaUK <anna.karpycheva@gmail.com>
2025-07-10 17:14:48 +0100

    Implement Octopus API product loading and UI display (1 step)

--- Pages/OctopusTest.cshtml.cs
+++ Pages/OctopusTest.cshtml.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using EnergyTariffAdvisor.OctopusApi;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace EnergyTariffAdvisor.Pages
 {
@@ -7,6 +9,7 @@ namespace EnergyTariffAdvisor.Pages
     {
         private readonly OctopusTariffService _octopusService;
 
+        // Список продуктов для отображения
         public List<ProductDto> Products { get; set; } = new();
 
         public OctopusTestModel(OctopusTariffService octopusService)
@@ -17,7 +20,7 @@ namespace EnergyTariffAdvisor.Pages
         public async Task OnGetAsync()
         {
             var response = await _octopusService.GetProductsAsync();
-            if (response != null)
+            if (response != null && response.Results != null)
             {
                 Products = response.Results;
             }
